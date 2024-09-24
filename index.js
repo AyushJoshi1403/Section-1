c7781a47ddfec14c8bdeb69ee0ac21525f6ad231
AyushJoshi1403 <ayush.aj1403@gmail.com>
2024-09-24 17:30:13 +0530

    conditions,loops in js

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log('Hello JavaScript!');
+
+a = 234;
+
+console.log(a);
+console.log(typeof a);
+
+message = "Be Cool";
+
+console.log(message);
+console.log(typeof message);
+
+isHungry = true;
+console.log(isHungry);
+
+[x,y,z] = [2,3,4];
+
+console.log(x+y+z);
+
+//exponent operator
+console.log(2**4);
+
+console.log( 1+1 );
+console.log( 1+'1' );
+console.log( 1+'1'+1 );
+console.log( 1+1+'1' );
+console.log( null+'1' );
+console.log( 1 == 1 );
+console.log( 1 == '1' );
+console.log( 1 === '1');
