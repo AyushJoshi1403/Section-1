fd6ce956b61d2861a031f03582c12edc0b0c4231
AyushJoshi1403 <ayush.aj1403@gmail.com>
2024-10-04 17:17:30 +0530

    objects

--- objects.js
+++ objects.js
@@ -40,3 +40,90 @@ console.log(smartPhones);
 //add a new color 'Green'
 smartPhones.colors.push('Green');
 console.log(smartPhones);
+
+console.log('-----');
+
+const smartphoneArray = [
+    {
+        brand : 'Samsung',
+        model : 'Galaxy M31',
+        price : 13999,
+        colors : ['Black', 'Blue', 'Red'],
+    },
+    {
+        brand : 'OnePlus',
+        model : '8T',
+        price : 42999,
+        colors : ['Black', 'Silver'],
+    },
+    {
+        brand : 'Apple',
+        model : 'iPhone 12',
+        price : 79900,
+        colors : ['Black', 'White', 'Green'],
+    },
+    {
+        brand : 'Xiaomi',
+        model : 'Redmi Note 9 Pro',
+        price : 15999,
+        colors : ['Black', 'Blue', 'Red'],
+    },
+    {
+        brand : 'Realme',
+        model : 'Narzo 20 Pro',
+        price : 18999,
+        colors : ['Black', 'White', 'Green'],
+    },
+];
+
+console.log('-----');
+
+console.log(smartphoneArray.length);
+
+console.log('-----');
+
+console.log(smartphoneArray[1].price);
+
+console.log('-----');
+
+// access 2nd color of third phone
+console.log(smartphoneArray[2].colors[1]);
+
+console.log('-----');
+
+// replace the first color of 4th phone with 'Silver'
+smartphoneArray[3].colors[0]= 'Silver'
+console.log(smartphoneArray[3]);
+
+console.log('-----');
+
+//add a new color 'yellow' to 5th phone
+smartphoneArray[4].colors.push('Yellow')
+console.log(smartphoneArray[4]);
+
+console.log('-----');
+
+const budgetPhones = smartphoneArray.filter( ( phone ) => {return phone.price > 15000 && phone.price < 20000 } );
+console.log(budgetPhones);
+
+console.log('-----');
+
+// display only brands
+const brands = smartphoneArray.map((phone) => { return phone.brand })
+console.log(brands);
+
+console.log('-----');
+
+// filter all phones having 'Black' color
+
+const blackPhones = smartphoneArray.filter( (phone) => {return phone.colors.includes('Black') } )
+console.log(blackPhones);
+
+console.log('-----');
+
+// filter all samsung phones
+
+const samsungPhones = smartphoneArray.filter( (phone) => {return phone.brand.includes('Samsung') } )
+console.log(samsungPhones);
+
+console.log('-----');
