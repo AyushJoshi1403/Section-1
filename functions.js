1b3c9a30f44f8fa028003f25228fb5789aab552b
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-07-31 23:22:11 +0530

    practise

--- functions.js
+++ functions.js
@@ -1,23 +1,23 @@
-function addNums(a,b){
-    var c = a+b;
+function addNums(a, b) {
+    var c = a + b;
 
     console.log(c);
 }
 
-addNums(34,21);
+addNums(34, 21);
 
 // console.log(c);
 
 console.log('-----');
 
-const getAvg = function(m1,m2,m3){
-    var avg = (m1+m2+m3)/3;
+const getAvg = function (m1, m2, m3) {
+    var avg = (m1 + m2 + m3) / 3;
     // console.log(avg);
 
     return avg;
 }
 
-const result = getAvg(782,353,782);
+const result = getAvg(782, 353, 782);
 
 console.log(result);
 
@@ -26,7 +26,7 @@ console.log('-----');
 const factorial = (n) => {
     let fact = 1;
 
-    for(let i=1; i<=n; i++){
+    for (let i = 1; i <= n; i++) {
         fact = fact * i;
     }
 
