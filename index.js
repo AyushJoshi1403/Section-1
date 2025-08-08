3b642e2822a1891d3b18e97e27da8f346e702a80
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-08-09 00:02:44 +0530

    practise

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ console.log(typeof message);
 isHungry = true;
 console.log(isHungry);
 
-[x,y,z] = [2,3,4];
+[x, y, z] = [2, 3, 4];
 
-console.log(x+y+z);
+console.log(x + y + z);
 
 //exponent operator
-console.log(2**4);
+console.log(2 ** 4);
 
-console.log( 1+1 );
-console.log( 1+'1' );
-console.log( 1+'1'+1 );
-console.log( 1+1+'1' );
-console.log( null+'1' );
-console.log( 1 == 1 );
-console.log( 1 == '1' );
-console.log( 1 === '1');
+console.log(1 + 1);
+console.log(1 + '1');
+console.log(1 + '1' + 1);
+console.log(1 + 1 + '1');
+console.log(null + '1');
+console.log(1 == 1);
+console.log(1 == '1');
+console.log(1 === '1');
