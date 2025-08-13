f497ad8b87ef5bfd38287d39edaf4109f3cd3324
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-08-13 15:08:51 +0530

    practise

--- loop_exercise.js
+++ loop_exercise.js
@@ -5,8 +5,8 @@ let [a, b] = [0, 1];
 console.log(a);
 console.log(b);
 
-for(let i=0; i<8; i++){
-    let c =a+b;
+for (let i = 0; i < 8; i++) {
+    let c = a + b;
     console.log(c);
     [a, b] = [b, c]
 }
@@ -14,16 +14,16 @@ for(let i=0; i<8; i++){
 console.log('-----');
 
 let n = 234;
-let sqrt = n**0.5
-if(Number.isInteger(sqrt)){
+let sqrt = n ** 0.5
+if (Number.isInteger(sqrt)) {
     console.log("Perfect square");
 }
 
 // WAP to print all perfect squares in range of 50 to 1000;
 
-for(let i=50; i<=1000; i++){
-    let sqrt = i**0.5
-    if(Number.isInteger(sqrt)){
+for (let i = 50; i <= 1000; i++) {
+    let sqrt = i ** 0.5
+    if (Number.isInteger(sqrt)) {
         console.log(i);
     }
 }
@@ -32,19 +32,19 @@ console.log('-----');
 
 // WAP to print all prime numbers in range of 50 to 1000;
 
-for(let num=50; num<=1000; num++){
-    
+for (let num = 50; num <= 1000; num++) {
+
     let prime = true;
 
-    for(let i=2; i<num; i++){
-        if(num % i === 0){
-        // console.log('not prime');
-        prime = false;
-        break;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            // console.log('not prime');
+            prime = false;
+            break;
         }
     }
 
-    if(prime) console.log(num);
+    if (prime) console.log(num);
 }
 
 console.log('-----');
