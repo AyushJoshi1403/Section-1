b538d0c3ad14576528ea475aff073351459d578a
AyushJoshi1403 <ayush.aj1403@gmail.com>
2025-08-15 23:58:52 +0530

    practise

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 // for, while, do-while
 
-for(let i=0; i<10; i++){
+for (let i = 0; i < 10; i++) {
     console.log(i);
 }
 
@@ -8,26 +8,26 @@ console.log('-----');
 
 let a = 10;
 
-while(a<20){
+while (a < 20) {
     console.log(a);
-    a+=2;
+    a += 2;
 }
 
 console.log('-----');
 
 let b = 20;
 
-do{
+do {
     console.log(b);
-    b+=2;
-}while(b<20);
+    b += 2;
+} while (b < 20);
 
 console.log('-----');
 
 // print all even number from 100 to 200
 
-for(let i=100; i<200; i++){
-    if(i%2===0)
+for (let i = 100; i < 200; i++) {
+    if (i % 2 === 0)
         console.log(i);
 }
 
@@ -38,15 +38,15 @@ console.log('-----');
 const num = 11;
 let prime = true;
 
-for(let i=2; i<num; i++){
-    if(num % i === 0){
+for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
         console.log('not prime');
         prime = false;
         break;
     }
 }
 
-if(prime) console.log('prime');
+if (prime) console.log('prime');
 
 console.log('-----');
 
@@ -55,12 +55,12 @@ console.log('-----');
 let n1 = 826375;
 let reverse = 0;
 
-while(n1>0){
+while (n1 > 0) {
     console.log(n1, reverse);
-    
-    const d = n1%10;
-    reverse = reverse*10 + d;
-    n1 = parseInt(n1/10);
+
+    const d = n1 % 10;
+    reverse = reverse * 10 + d;
+    n1 = parseInt(n1 / 10);
 }
 
 console.log(n1, reverse);
